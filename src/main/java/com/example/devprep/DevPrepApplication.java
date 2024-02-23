09fe03565ea924d96f90337f26c3241363a0eba8
수현 <su551732@gmail.com>
2024-02-23 22:54:27 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/devprep/DevPrepApplication.java
@@ -0,0 +1,13 @@
+package com.example.devprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevPrepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevPrepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devprep/DevPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevPrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
