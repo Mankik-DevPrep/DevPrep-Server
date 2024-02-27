cdae94ec0fed57e82aff922c27e94bc7af2c0365
SIWON990327 <pos06058@naver.com>
2024-02-27 16:17:09 +0900

    [CHORE]/#11-Service 인터페이스 수정

--- src/main/java/com/example/devprep/resume/ResumeService.java
+++ src/main/java/com/example/devprep/resume/ResumeService.java
@@ -2,7 +2,7 @@ package com.example.devprep.resume;
 
 public interface ResumeService {
 
-    public ResumeDto.resumeResponseDto createResume(ResumeDto.resumeRequestDto createResumeRequestDto);
+    ResumeDto.resumeResponseDto createResume(ResumeDto.resumeRequestDto createResumeRequestDto);
 
-    public ResumeDto.resumeResponseDto updateResume(ResumeDto.resumeRequestDto updateResumeRequestDto, Long id);
+    ResumeDto.resumeResponseDto updateResume(ResumeDto.resumeRequestDto updateResumeRequestDto, Long id);
 }
