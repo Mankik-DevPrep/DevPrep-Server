0fc02cca1999f81ea01bcc34363ccee66c0ecf4d
SIWON990327 <pos06058@naver.com>
2024-02-27 15:39:53 +0900

    [CHORE]/#7-Resume 엔드포인트 변경

--- src/main/java/com/example/devprep/resume/ResumeController.java
+++ src/main/java/com/example/devprep/resume/ResumeController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/resume")
+@RequestMapping("/resumes")
 public class ResumeController {
 
     private final ResumeService resumeService;
 
-    @PostMapping("/create")
+    @PostMapping("/resume")
     public ResponseEntity<?> createResume(@RequestBody ResumeDto.createResumeRequestDto request) {
         return ResponseEntity.ok().body(resumeService.createResume(request));
     }
